7ac989bae993fad54f7e03389f959f5d6d5270b3
linguanghuan <linguanghuan@gmail.com>
2018-11-27 22:47:48 +0800

    role表缺少了app_id字段修复

--- auth-server/auth-server-core/src/main/resources/sql/role/role.sql
+++ auth-server/auth-server-core/src/main/resources/sql/role/role.sql
@@ -1,10 +1,11 @@
-CREATE TABLE `role`(
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
-  `gmt_create` TIMESTAMP NOT NULL  DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
-  `gmt_modified` TIMESTAMP NOT NULL DEFAULT now() ON UPDATE now() COMMENT '修改时间',
-  `role_name` VARCHAR (255) NOT NULL COMMENT '角色名称',
-  `descr` VARCHAR (255) NOT NULL COMMENT '描述',
-  `resource_ids` VARCHAR (255) COMMENT '资源ids',
-  `available` TINYINT (1) COMMENT '是否可用',
+CREATE TABLE `role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `gmt_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `app_id` bigint(20) NOT NULL,
+  `role_name` varchar(255) NOT NULL COMMENT '角色名称',
+  `descr` varchar(255) NOT NULL COMMENT '描述',
+  `resource_ids` varchar(255) DEFAULT NULL COMMENT '资源ids',
+  `available` tinyint(1) DEFAULT NULL COMMENT '是否可用',
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB  DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
